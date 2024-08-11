5a08af6936dde4f599afbaf34009761b9731ff25
Gayatri <gayatri.harriee@gmail.com>
2024-08-11 11:09:22 -0400

    Wrote code for FizzBuzz problem

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,10 @@
 # add your code here
+number = 1
+for number in range(1,101):
+    if number % 3 == 0 and number % 5 != 0:
+        print("Fizz")
+    elif number % 5 == 0 and number % 3 != 0:
+        print("Buzz")
+    else:
+        print(number)
 
